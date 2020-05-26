62f73567e70d34bd85a51a902f67cbdc1c7ad026
Ardo-Tang <z71932028@gmail.com>
2020-05-26 20:38:55 +0800

    my eigen3 first test

--- scan_matching/apps/01_eigen_tester.cpp
+++ scan_matching/apps/01_eigen_tester.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <Eigen/Dense>
 
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    Eigen::Vector2d a(5.0,6.0);
+    Eigen::Vector2d a(5.0, 6.0);
+    Eigen::Vector2d b(2.0, 8.0);
+    cout << (a + b).transpose() << endl;
     return 0;
 }
---
+++ scan_matching/build/CMakeFiles/01_eigen_tester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01_eigen_tester.dir/apps/01_eigen_tester.cpp.o"
+  "01_eigen_tester.pdb"
+  "01_eigen_tester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_eigen_tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
